e8c1d82714433ab1d8060d298dccc7a94876a8ae
Thejaunt <119899718+Thejaunt@users.noreply.github.com>
2023-02-09 19:54:45 +0200

    Created Task model linked by many-to-one relation with Django's default User model. Created superuser

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -1,16 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    ...
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
-        db_table = 'user'
-
-
-class Todo_list(models.Model):
-    ...
-
-
-    class Meta:
-        db_table = 'todo_list'
+        ordering = ['done']
