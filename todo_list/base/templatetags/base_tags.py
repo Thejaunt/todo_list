100ef6609eecf1a9e4edb1ef8857074f8a8a1180
Thejaunt <119899718+Thejaunt@users.noreply.github.com>
2023-02-20 20:13:58 +0200

    Added type hinting to methods in custom templatetags. that wasn't complete at the previous commit

--- todo_list/base/templatetags/base_tags.py
+++ todo_list/base/templatetags/base_tags.py
@@ -1,26 +1,27 @@
 from django import template
+from base.models import Task
 
 register = template.Library()
-
-
-@register.filter(name='spent_to_finish')
-def spent_to_finish(obj) -> str:
-    return __get_delta_to_str(__get_timedelta(obj))
-
-
-def __get_timedelta(obj) -> dict:
-    td = obj.done_at - obj.created_at
-    hours = (td.seconds // 3600) + (td.days * 24)
-    minutes = (td.seconds // 60) % 60
-    return {'hours': hours, 'minutes': minutes}
 
 
 def __get_delta_to_str(hours_minutes: dict) -> str:
     return f'{hours_minutes["hours"]}h {hours_minutes["minutes"]}m'
 
 
+def __get_timedelta(obj: type[Task]) -> dict:
+    td = obj.done_at - obj.created_at
+    hours = (td.seconds // 3600) + (td.days * 24)
+    minutes = (td.seconds // 60) % 60
+    return {'hours': hours, 'minutes': minutes}
+
+
+@register.filter(name='spent_to_finish')
+def spent_to_finish(obj: type[Task]) -> __get_delta_to_str:
+    return __get_delta_to_str(__get_timedelta(obj))
+
+
 @register.filter(name='avg_time')
-def get_avg_time(obj) -> str:
+def get_avg_time(obj: list[type[Task]]) -> __get_delta_to_str:
     minutes = 0
     for item in obj:
         hm = __get_timedelta(item)
